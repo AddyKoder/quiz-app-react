7f34d8e58b46c89bd498073b10415851549f8923
Aditya Tripathi <productive.py@gmail.com>
2022-10-21 13:45:15 +0530

    added pwa support

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './Components/App.jsx'
-
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 
 // Default react index.js file
@@ -11,3 +11,6 @@ root.render(
     <App/>
   </React.StrictMode>
 );
+
+
+serviceWorkerRegistration.register();
